cf1c5238572afdaf8ba1caeb54105f023ca01308
Navneet Juneja <navneet.juneja@cybergindia.local>
2019-04-02 11:24:06 +0530

    resolved white screen issue

--- android/app/src/main/java/com/rncourse/MainApplication.java
+++ android/app/src/main/java/com/rncourse/MainApplication.java
@@ -35,4 +35,8 @@ public class MainApplication extends NavigationApplication  {
         return getPackages();
     }
    
+    @Override
+public String getJSMainModuleName() {
+	return "index.android.js";
+}
 }
